04761e444b1276d0e1959efddb128e6eb3eb5f0e
Alexandr Sinev <sir.sineow@yandex.ru>
2024-01-23 22:30:54 +0300

    Реализован класс Channel, реализован метод print_info для вывода в консоль данных о youtube кнале

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,29 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+import isodate
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
+    def get_response(self):
+        request = self.youtube.channels().list(
+            part="snippet,contentDetails,statistics",
+            id=self.channel_id
+        )
+        response = request.execute()
+
+        return response
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(Channel.get_response(self), indent=2, ensure_ascii=False))
