5a6ae54c05aeb6bf3fdc5fe4572a860f4ad34c82
Alexandr Sinev <sir.sineow@yandex.ru>
2024-02-15 00:45:36 +0300

    В класс Video добавлен блок try/except для перехвата исключения появляющегося при передаче пользователем несуществующего id видео

--- src/video.py
+++ src/video.py
@@ -1,17 +1,24 @@
 import json
 from src.channel import Channel
+from src.mixin import APIMixin
 
 
-class Video(Channel):
+class Video(Channel, APIMixin):
 
     def __init__(self, video_id):
         """Экземпляр инициализируется id видео."""
-
-        self.video_id = video_id
-        self.title = self.get_attributes()['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count = self.get_attributes()['items'][0]['statistics']['viewCount']
-        self.number_of_likes = self.get_attributes()['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.title = self.get_attributes()['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = self.get_attributes()['items'][0]['statistics']['viewCount']
+            self.like_count = self.get_attributes()['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return f'{self.title}'
@@ -19,9 +26,9 @@ class Video(Channel):
     def get_response(self):
         """Получаем статистику видео по его id"""
 
-        video_response = super().get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=self.video_id
-                                                             ).execute()
+        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=self.video_id
+                                                          ).execute()
 
         return video_response
 
@@ -33,7 +40,7 @@ class Video(Channel):
         return new_file
 
 
-class PLVideo(Channel):
+class PLVideo(Channel, APIMixin):
     def __init__(self, video_id, playlist_id):
         """Экземпляр инициализируется id видео и id плейлиста."""
 
@@ -50,9 +57,9 @@ class PLVideo(Channel):
     def get_response(self):
         """Получаем статистику видео по его id"""
 
-        video_response = super().get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=self.video_id
-                                                             ).execute()
+        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=self.video_id
+                                                          ).execute()
 
         return video_response
 
@@ -62,3 +69,4 @@ class PLVideo(Channel):
         file = json.dumps(self.get_response(), indent=2, ensure_ascii=False)
         new_file = json.loads(file)
         return new_file
+
